6daca146e1ba92d5acf064f20fa7f0ed667e3b9e
yutatakabatake <yutatakabatake@gmail.com>
2024-01-24 11:57:25 -0800

    print error

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,11 @@
 let _ = 
     let lexbuf = Lexing.from_channel stdin in
+    try
         let rlt = Temple_parser.prog Temple_lexer.token lexbuf in
             Temple_asm.print_record2 rlt;
             Temple_asm.print_record3 rlt
+    with exn ->
+        begin
+            print_string (Lexing.lexeme lexbuf);
+            raise exn
+        end
