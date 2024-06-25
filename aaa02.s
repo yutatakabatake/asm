5e51fc631fd0d0d12028189273613de85437f5e6
yutatakabatake <yutatakabatake@gmail.com>
2024-06-25 15:11:56 +0900

    コメント部分をプリント

--- aaa02.s
+++ aaa02.s
@@ -1,1 +1,24 @@
-SETIN 1000 MOVE $m0 SETIL haji_me MOVE $m1 JL $m2 100 $m3
+SETI 1000
+MOVE $m0
+LD $m0
+MOVE $m1
+
+SETI 1002
+MOVE $m2
+LD $m2
+ADD $m1
+MOVE $m3
+
+SETI 1004
+MOVE $m4
+SETI 0
+ADD $m3
+SD $m4
+
+// SETI 1006
+// MOVE $m5
+// SETI 2
+// ADD $m3 
+// SD $m5
+
+JL $m31 111 $m28
